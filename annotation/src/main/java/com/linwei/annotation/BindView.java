9517cc384d76dd9a44b4fb2404c0bce02578bf29
lw <linwei9605@gmail.com>
2020-04-29 09:54:10 +0800

    创建注解

---
+++ annotation/src/main/java/com/linwei/annotation/BindView.java
@@ -0,0 +1,17 @@
+package com.linwei.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Author: weiyun
+ * @Time: 2020/4/28
+ * @Description: ButterKnife中View绑定注解
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD})
+public @interface BindView {
+    int value();  //View id
+}
---
+++ annotation/src/main/java/com/linwei/annotation/OnClick.java
@@ -0,0 +1,9 @@
+package com.linwei.annotation;
+
+/**
+ * @Author: weiyun
+ * @Time: 2020/4/28
+ * @Description: ButterKnife中View绑定注解
+ */
+public @interface OnClick {
+}
